d1776455fa69c92dfa2eb4f0d8ddbff714435b6d
Gsyyyyy <109256970+Gsyyyyy@users.noreply.github.com>
2023-04-21 23:52:16 +0100

    Add files via upload

---
+++ 三数之和.py
@@ -0,0 +1,67 @@
+'''
+给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请
+
+你返回所有和为 0 且不重复的三元组。
+
+注意：答案中不可以包含重复的三元组。
+
+ 
+
+ 
+
+示例 1：
+
+输入：nums = [-1,0,1,2,-1,-4]
+输出：[[-1,-1,2],[-1,0,1]]
+解释：
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0 。
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0 。
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
+不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
+注意，输出的顺序和三元组的顺序并不重要。
+示例 2：
+
+输入：nums = [0,1,1]
+输出：[]
+解释：唯一可能的三元组和不为 0 。
+示例 3：
+
+输入：nums = [0,0,0]
+输出：[[0,0,0]]
+解释：唯一可能的三元组和为 0 。
+
+算法流程：
+特判，对于数组长度 nn，如果数组为 nullnull 或者数组长度小于 33，返回 [][]。
+对数组进行排序。
+遍历排序后数组：
+若 nums[i]>0nums[i]>0：因为已经排序好，所以后面不可能有三个数加和等于 00，直接返回结果。
+对于重复元素：跳过，避免出现重复解
+令左指针 L=i+1L=i+1，右指针 R=n-1R=n−1，当 L<RL<R 时，执行循环：
+当 nums[i]+nums[L]+nums[R]==0nums[i]+nums[L]+nums[R]==0，执行循环，判断左界和右界是否和下一位置重复，去除重复解。并同时将 L,RL,R 移到下一位置，寻找新的解
+若和大于 00，说明 nums[R]nums[R] 太大，RR 左移
+若和小于 00，说明 nums[L]nums[L] 太小，LL 右移
+
+'''
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        nums.sort()
+        if len(nums)<3:
+            return result
+        for i in range(len(nums)-2):
+            j = len(nums)-1
+            z = i+1
+            if nums[i]>0:
+                break
+            while z<j:
+                if nums[i]+nums[z]+nums[j] == 0:
+                    list1=[nums[i],nums[z],nums[j]]
+                    if result.count(list1) == 0:
+                        result.append(list1)
+                    z+=1
+                    j-=1
+                else:
+                    if nums[i]+nums[z]+nums[j] > 0:
+                        j-=1
+                    else:z+=1
+        return result
